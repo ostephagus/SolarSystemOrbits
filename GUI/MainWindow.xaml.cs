db8692aff4473ea0f7365e87a5178722377cd6bf
ostephagus <37499766+ostephagus@users.noreply.github.com>
2023-05-10 09:38:27 +0100

    Reactive font size of all button text now the same across the buttons

--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +19,30 @@ namespace GUI
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private int reactiveFontSize;
+        public int ReactiveFontSize { get { return reactiveFontSize; } 
+            set {
+                reactiveFontSize = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(reactiveFontSize)));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private const int fontSizeDivisor = 50;
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+            ReactiveFontSize = (int)ActualWidth / fontSizeDivisor;
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ReactiveFontSize = (int)ActualWidth / fontSizeDivisor;
         }
     }
 }
