82f4b43b7ee57b8264524547777d2b009c37d83a
ostephagus <37499766+ostephagus@users.noreply.github.com>
2023-05-11 09:07:56 +0100

    Hopefully this doesn't break things?

---
+++ GUI/Challenge1.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GUI
+{
+    /// <summary>
+    /// Interaction logic for Challenge1.xaml
+    /// </summary>
+    public partial class Challenge1 : Window
+    {
+        public Command_CloseWindow Command_CloseWindow { get; } = new Command_CloseWindow();
+
+        public Challenge1()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Command_CloseWindow.Execute(this); //On click of CloseButton, execute the CloseWindow command with the current window as parameter.
+        }
+    }
+}
--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -22,14 +22,17 @@ namespace GUI
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
         private int reactiveFontSize;
-        public int ReactiveFontSize { get { return reactiveFontSize; } 
+        public int ReactiveFontSize { 
+            get { return reactiveFontSize; } 
             set {
                 reactiveFontSize = value;
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(reactiveFontSize)));
             }
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public Command_ChangeToWindow1 Command_ChangeToWindow1 { get; } = new Command_ChangeToWindow1();
 
         private const int fontSizeDivisor = 50;
 
@@ -37,12 +40,66 @@ namespace GUI
         {
             InitializeComponent();
             DataContext = this;
-            ReactiveFontSize = (int)ActualWidth / fontSizeDivisor;
+            if (ActualWidth > 0)
+            {
+                ReactiveFontSize = (int)ActualWidth / fontSizeDivisor;
+            }
+            else
+            {
+                ReactiveFontSize = 16;
+            }
         }
 
         private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
         {
             ReactiveFontSize = (int)ActualWidth / fontSizeDivisor;
+        }
+    }
+
+    public class Command_ChangeToWindow1 : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter) //Return true since the command can always execute
+        {
+            return true;
+        }
+
+        public void Execute(object parameter) //Show a new instance of Challenge1
+        {
+            Challenge1 challengeWindow = new Challenge1();
+            Window mainWindow = Application.Current.MainWindow;
+
+            // Set the position of the Challenge1 window to match the position of the MainWindow
+            challengeWindow.Left = mainWindow.Left;
+            challengeWindow.Top = mainWindow.Top;
+
+            // Close the MainWindow and show the Challenge1 window
+            mainWindow.Close();
+            challengeWindow.Show();
+        }
+    }
+    public class Command_CloseWindow : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter) //Return true since the command can always execute
+        {
+            return true;
+        }
+
+        // Define the logic to execute when the command is executed
+        public void Execute(object parameter)
+        {
+            Window currentWindow = (Window)parameter;
+            MainWindow mainWindow = new MainWindow();
+            //Set the position of the MainWindow to match the position of the previous Challenge window
+            mainWindow.Left = currentWindow.Left;
+            mainWindow.Top = currentWindow.Top;
+
+            // Close the current window and show the MainWindow
+            currentWindow.Close();
+            mainWindow.Show();
         }
     }
 }
