b1dfadb3b1a924540b56333cf2dbc151605cebd5
ostephagus <37499766+ostephagus@users.noreply.github.com>
2023-05-15 11:33:06 +0100

    Started migrating _Click subroutines to command calls

--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -32,7 +32,7 @@ namespace GUI
 
         public event PropertyChangedEventHandler? PropertyChanged;
 
-        private Command_ChangeToWindow1 Command_ChangeToWindow1 { get; } = new Command_ChangeToWindow1();
+        public ICommand Command_ChangeToWindow1 { get; } = new Command_ChangeToWindow1();
 
         private const int fontSizeDivisor = 50;
 
